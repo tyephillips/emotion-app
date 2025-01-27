c78bea3caf08f4d4cf1090a0757f007792c0ed6c
Tye <tyephillips47@gmail.com>
2025-01-27 18:21:04 +0200

    Added updates to templates and views

--- emotions/views.py
+++ emotions/views.py
@@ -3,28 +3,17 @@ from .models import Emotion
 
 
 def get_affirmations(request):
-    """
-    Retrieves affirmations based on the selected emotion.
-    
-    This view fetches all available emotions and displays them in a dropdown form.
-    If the user selects an emotion and submits the form, it fetches the related affirmations.
-    
-    Args:
-        request (HttpRequest): The HTTP request object that contains the submitted form data.
+    emotions = Emotion.objects.all()  # Fetch all emotions
+    affirmations = []
 
-    Returns:
-        HttpResponse: A rendered template containing the emotion selection form and relevant affirmations.
-    """
-    emotions = Emotion.objects.all()
-    
-    if request.method == 'POST':
-        # Get the selected emotion from the form data
-        selected_emotion = request.POST.get('emotion')
-        # Retrieve the affirmations associated with the selected emotion
-        affirmations = selected_emotion.affirmations.all() if selected_emotion else []
-    else:
-        affirmations = []
+    selected_emotion_name = request.GET.get('emotion')  # Get emotion name from query params
+    if selected_emotion_name:
+        # Fetch the corresponding Emotion object
+        emotion = Emotion.objects.filter(name=selected_emotion_name).first()
+        if emotion:
+            affirmations = emotion.affirmations.all()  # Fetch affirmations for the emotion
 
-    return render(request, 'emotions/affirmations.html', {'emotions': emotions,
-                                             'affirmations': affirmations})
-
+    return render(request, 'emotions/affirmations.html', {
+        'emotions': emotions,
+        'affirmations': affirmations,
+    })
